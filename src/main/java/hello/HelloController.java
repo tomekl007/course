a049b0a3821c957a68417d97fab6e343fa985a11
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-07 22:06:48 +0100

    add db fetch and save

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,10 +1,27 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class HelloController {
+
+
+    private RecordRepository repository;
+
+    @Autowired
+    public HelloController(RecordRepository repository) {
+        this.repository = repository;
+    }
+
 
     @RequestMapping("/")
     public String index() {
@@ -14,6 +31,26 @@ public class HelloController {
     @RequestMapping("/a")
     public String a() {
         return "Greetings from Spring Boot!";
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, path = "/db")
+    public String home(ModelMap model) {
+        List<Record> records = repository.findAll();
+        model.addAttribute("records", records);
+        model.addAttribute("insertRecord", new Record());
+        return "home";
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, path = "/db")
+    public String insertData(ModelMap model,
+                             @ModelAttribute("insertRecord") @Valid Record record,
+                             BindingResult result) {
+        if (!result.hasErrors()) {
+            repository.save(record);
+        }
+        return home(model);
     }
 
 }
